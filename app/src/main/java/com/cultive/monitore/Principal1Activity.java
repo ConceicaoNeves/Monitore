6d5319c2e80239a43a6ad0a22b468282bc483c44
Conceição Neves Silva <54824261+ConceicaoNeves@users.noreply.github.com>
2022-06-14 21:24:39 -0300

    new

---
+++ app/src/main/java/com/cultive/monitore/Principal1Activity.java
@@ -0,0 +1,14 @@
+package com.cultive.monitore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Principal1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal1);
+    }
+}
---
+++ app/src/main/java/com/cultive/monitore/Tutorial2Activity.java
@@ -0,0 +1,28 @@
+package com.cultive.monitore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Tutorial2Activity extends AppCompatActivity {
+    Button bt3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial2);
+
+        bt3 = (Button) findViewById(R.id.button3);
+
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Tutorial2Activity.this,
+                        Tutorial3Activity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
