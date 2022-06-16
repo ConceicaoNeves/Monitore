ebde3aa87f5a9efd7628806dc960e53afa9a7bd2
Conceição Neves Silva <54824261+ConceicaoNeves@users.noreply.github.com>
2022-06-15 23:11:12 -0300

    add login with google

--- app/src/main/java/com/cultive/monitore/MainActivity.java
+++ app/src/main/java/com/cultive/monitore/MainActivity.java
@@ -1,22 +1,81 @@
 package com.cultive.monitore;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+    GoogleSignInOptions gop;
+    GoogleSignInClient goc;
+    ImageView googleBtn;
+
     Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bt = (Button) findViewById(R.id.button);
+        googleBtn = findViewById(R.id.google_btn);
+
+        gop= new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        goc = GoogleSignIn.getClient(this, gop);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account!=null){
+            navigateToSecondActivity();
+        }
+
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+
+
+    }
+
+    void signIn(){
+        Intent signInIntent = goc.getSignInIntent();
+        startActivityForResult(signInIntent,1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                task.getResult(ApiException.class);
+                navigateToSecondActivity();
+            } catch (ApiException e) {
+                Toast.makeText(getApplicationContext(), "Algo deu errado. Tente novamente!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+    void navigateToSecondActivity(){
+        finish();
+        Intent i = new Intent(MainActivity.this,
+                Tutorial1Activity.class);
+        startActivity(i);
+    }
+        /*bt = (Button) findViewById(R.id.button);
 
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +84,5 @@ public class MainActivity extends AppCompatActivity {
                         Tutorial1Activity.class);
                 startActivity(i);
             }
-        });
+        });*/
     }
-}
--- app/src/main/java/com/cultive/monitore/Tutorial1Activity.java
+++ app/src/main/java/com/cultive/monitore/Tutorial1Activity.java
@@ -7,12 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 public class Tutorial1Activity extends AppCompatActivity {
     Button bt2;
+    GoogleSignInOptions gop;
+    GoogleSignInClient goc;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tutorial1);
+
+        gop = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        goc = GoogleSignIn.getClient(this,gop);
 
         bt2 = (Button) findViewById(R.id.button2);
 
