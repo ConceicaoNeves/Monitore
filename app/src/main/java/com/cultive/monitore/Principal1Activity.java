10aa2a13a8243fe50f605a3387931563105e3208
Conceição Neves Silva <54824261+ConceicaoNeves@users.noreply.github.com>
2022-06-17 16:35:27 -0300

    add tela luminosidade com firebase

--- app/src/main/java/com/cultive/monitore/Principal1Activity.java
+++ app/src/main/java/com/cultive/monitore/Principal1Activity.java
@@ -2,13 +2,29 @@ package com.cultive.monitore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Principal1Activity extends AppCompatActivity {
-
+    Button bt5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal1);
+
+        bt5 = (Button) findViewById(R.id.button5);
+
+        bt5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Principal1Activity.this,
+                        Temp.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 }
