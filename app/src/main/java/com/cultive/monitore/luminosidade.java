25ef3ed6b98276d11d3d206fdfdc83fb8bb2ff56
Conceição Neves Silva <54824261+ConceicaoNeves@users.noreply.github.com>
2022-06-18 10:50:39 -0300

    remove button luz

--- app/src/main/java/com/cultive/monitore/luminosidade.java
+++ app/src/main/java/com/cultive/monitore/luminosidade.java
@@ -13,11 +13,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class luminosidade extends AppCompatActivity {
 
     TextView txt;
-
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    
     private DatabaseReference bancoDataref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
